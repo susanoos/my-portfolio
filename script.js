677946d21fc19673e39a0fb35580224a3079ddb8
David <escxbar@gmail.com>
2023-02-03 09:08:41 -0800

    updated code

--- script.js
+++ script.js
@@ -12,21 +12,21 @@ const projects = [
     description:
       "Virtual vending machine application with command-line interface allowing users to deposit money, built during my time with Merit America and fellow students.",
     img: "https://media4.giphy.com/media/fph1vX16qKDkiz7BnI/giphy.gif?cid=790b7611b80c108f689c67c7e358ec75bc2791544189f1c3&rid=giphy.gif&ct=g",
-    liveDemo: "No demo",
+    liveDemo: "",
     codeRepository:
       "https://github.com/susanoos/Merit-America-Module-1-Capstone-VendingMachine/tree/main",
   },
   {
     title: "Bankist",
-    description: `Taught in the JavaScript course I took in Udemy, it was used as a tutorial to implament various features which are frequently used today.`,
+    description: `Taught in the JavaScript course I took on Udemy, it was used as a tutorial to implament various features which are frequently used today.`,
     img: "./imgs/bankist.png",
     liveDemo: "https://susanoos.github.io/bankist-landing/",
     codeRepository: "https://github.com/susanoos/bankist-landing",
   },
   {
     title: "Pokédex",
-    description: `A simple application that allows the user to search for a Pokémon by name.
-    and retrieve information about that Pokémon.`,
+    description:
+      "A simple application that allows the user to search for a Pokémon by name. and retrieve information about that Pokémon. (Still under development)",
     img: "./imgs/pokedex.png",
     liveDemo: "https://susanoos.github.io/pokedex/",
     codeRepository: "https://github.com/susanoos/pokedex",
@@ -57,7 +57,7 @@ const projectsHTML = projects
               ${project.description}
             </p>
           <div class="project-links">
-            <a href="live-demo-link" target="_blank">Demo: ${project.liveDemo}</a>
+            <a href="${project.liveDemo}" target="_blank">Demo: ${project.liveDemo}</a>
             <a href="${project.codeRepository}" target="_blank"><i class="fa-brands fa-github icon"></i></a>
             </div>
           </div>
